1ef615aeb32e6734306615c3354836799cdd6387
jr1412594 <jr.medina1412@gmail.com>
2020-10-19 11:30:24 -0600

    user model, index to destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+u1 = User.create(name: "Bob Dobbolina", weight: 175.5, activity_level: 5, goal: "Get stronger", age: 22)
+u1 = User.create(name: "Mary Poppins", weight: 132.8, activity_level: 9, goal: "Get faster", age: 42)
+u1 = User.create(name: "Dwayne Johnson ", weight: 265.9, activity_level: 10, goal: "More cheat meals", age: 53)
